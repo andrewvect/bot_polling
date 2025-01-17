9243d846c2f8f53ee22b1c4c049938502c1e3950
andrewvect <andrewvect@gmail.com>
2025-01-17 12:09:18 +0000

    Add error handling for missing BOT_TOKEN environment variable

--- src/config.py
+++ src/config.py
@@ -2,7 +2,10 @@ import dotenv
 import os
 
 dotenv.load_dotenv()
+try:
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+except KeyError:
+    raise KeyError("BOT_TOKEN environment variable is not set")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
 PORT = int(os.getenv("PORT", 3000))
 WEBHOOK_URL = os.getenv("WEBHOOK_URL", 'webhook')
